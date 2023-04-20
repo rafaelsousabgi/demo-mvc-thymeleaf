4126d29ad77b6c091b5bfdcd9784a78f2f6a078e
Rafael Sousa <rafael_david.2@hotmail.com>
2023-04-20 14:38:26 -0300

    atualização e validação de campos

--- src/main/java/com/example/demo/curso/boot/domain/Departamento.java
+++ src/main/java/com/example/demo/curso/boot/domain/Departamento.java
@@ -6,12 +6,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @SuppressWarnings("serial")
 @Entity
 @Table(name = "DEPARTAMENTO")
 public class Departamento extends AbstractEntity<Long> {
 
+	@NotBlank(message="Informe um nome")
+	@Size(min = 3, max = 60, message ="O nome do departamento de ter entre (mim) e max (caractes.")
 	@Column(name = "nome", nullable = false , unique = true,length = 60)
 	private String nome;
 	
--- src/main/java/com/example/demo/curso/boot/web/controller/CargoController.java
+++ src/main/java/com/example/demo/curso/boot/web/controller/CargoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,8 @@ import com.example.demo.curso.boot.domain.Cargo;
 import com.example.demo.curso.boot.domain.Departamento;
 import com.example.demo.curso.boot.service.CargoService;
 import com.example.demo.curso.boot.service.DepartamentoService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/cargos")
@@ -40,7 +43,12 @@ public class CargoController {
 	}
 	
 	@PostMapping("/salvar")
-	public String salvar(Cargo cargo, RedirectAttributes attr) {
+	public String salvar(@Valid Cargo cargo,BindingResult result, RedirectAttributes attr) {
+		
+		if(result.hasErrors()) {
+			return"/cargo/cadastro";
+		}
+		
 		cargoService.salvar(cargo);
 		attr.addFlashAttribute("success", "Cargo inserido com sucesso");
 		return "redirect:/cargos/cadastrar";
@@ -53,7 +61,12 @@ public class CargoController {
 	}
 	
 	@PostMapping("/editar")
-	public String editar(Cargo cargo, RedirectAttributes attr) {
+	public String editar(@Valid Cargo cargo, BindingResult result ,RedirectAttributes attr) {
+		if(result.hasErrors()) {
+			return"/cargo/cadastro";
+		}
+		
+		
 		cargoService.editar(cargo);
 		attr.addFlashAttribute("success", "editado com sucesso");
 		return "redirect:/cargos/cadastrar";
